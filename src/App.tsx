9f6c1e14613d33e6dd6533d2e0f1f944cbac20c1
Arthur <arthurfellype21@gmail.com>
2023-02-15 13:46:03 -0300

    starting repository

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+
+ const App = () => {
+  return (
+    <View className=" flex-1 bg-orange-400 justify-center items-center">
+      <Text >Texto de teste449898</Text>
+      <StatusBar />
+    </View>
+  );
+}
+
+export default App
